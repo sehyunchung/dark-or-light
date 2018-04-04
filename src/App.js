6c59858354d137723ad83ee86b37cc2ecaa789e7
sxhyxnchxng <sxhyxnchxng@gmail.com>
2018-04-04 22:51:11 +0900

    기능 개선 - train data 추가

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ network.train([
   { input: { r: 0.74, g: 0.78, b: 0.86 }, output: { light: 1 } },
   { input: { r: 0.31, g: 0.35, b: 0.41 }, output: { dark: 1 } },
   { input: { r: 1, g: 0.99, b: 0 }, output: { light: 1 } },
-  { input: { r: 1, g: 0.42, b: 0.52 }, output: { dark: 1 } }
+  { input: { r: 1, g: 0.42, b: 0.52 }, output: { dark: 1 } },
+  { input: { r: 0.45, g: 0.5, b: 0.98 }, output: { dark: 1 } },
+  { input: { r: 0.22, g: 0.58, b: 0.98 }, output: { dark: 1 } },
+  { input: { r: 0.18, g: 0.56, b: 0.98 }, output: { dark: 1 } }
 ]);
 
 const getRgb = hex => {
